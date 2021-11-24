f0870900c94284ac21484d4b73b979554ce3efef
myeongjin1115 <myeongjin1115@gmail.com>
2021-11-24 14:27:15 +0900

    first commit

---
+++ smart-guide-security/src/main/java/com/example/smartguide/dto/GroupInfo.java
@@ -0,0 +1,10 @@
+package com.example.smartguide.dto;
+
+import lombok.Data;
+
+@Data
+public class GroupInfo {
+	private String large;
+	private String medium;
+	private String small;
+}
--- smart-guide-security/src/main/java/com/example/smartguide/dto/MyPageRes.java
+++ smart-guide-security/src/main/java/com/example/smartguide/dto/MyPageRes.java
@@ -1,15 +1,10 @@
 package com.example.smartguide.dto;
 
-import java.time.LocalDateTime;
-
 import lombok.Data;
 
 @Data
 public class MyPageRes {
 	private String username;
-	private String phoneNumber;
-	private String authority;
-	private String groupName;
-	private LocalDateTime createdAt;
-	private LocalDateTime updatedAt;
+	private String role;
+	private GroupInfo group;
 }

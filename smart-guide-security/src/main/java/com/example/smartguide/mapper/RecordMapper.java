4faf0623794390bc0b05eb564fed439953d99e46
myeongjin1115 <myeongjin1115@gmail.com>
2021-12-07 13:53:33 +0900

    FIX: manager home 에서 집계된 일간, 주간, 월간 방문자 수에서 중복된 인원을 하루를 기준으로  처리하도록 수정

--- smart-guide-security/src/main/java/com/example/smartguide/mapper/RecordMapper.java
+++ smart-guide-security/src/main/java/com/example/smartguide/mapper/RecordMapper.java
@@ -34,69 +34,57 @@ public interface RecordMapper {
 	int insertRecord(@Param("record") Record record);
 	
 	// 해당 빌딩에 오늘 하루 동안 기록된 record 수 및 인원 수
-	@Select("SELECT count(*), count(DISTINCT user_id) FROM record"
+	@Select("SELECT IFNULL(SUM(result.cnt), 0) AS record_count, COUNT(result.cnt) AS user_count FROM"
+			+ " (SELECT COUNT(*) AS cnt FROM record"
 			+ " WHERE DATE(created_at)=DATE(NOW())"
 			+ " AND beacon_id"
-			+ " IN (SELECT id FROM beacon WHERE building_id=#{buildingId})")
-	@Results({
-		@Result(property="recordCount", column="count(*)"),
-		@Result(property="userCount", column="count(DISTINCT user_id)")
-	})
+			+ " IN (SELECT id FROM beacon WHERE building_id=#{buildingId})"
+			+ " GROUP BY DATE(created_at)) AS result")
 	RecordCount selectDailyRecordCountByBuildingId(@Param("buildingId") Long buildingId);
 	
 	// 해당 빌딩에 어제 하루 동안 기록된 record 수 및 인원 수
-	@Select("SELECT count(*), count(DISTINCT user_id) FROM record"
+	@Select("SELECT IFNULL(SUM(result.cnt), 0) AS record_count, COUNT(result.cnt) AS user_count FROM"
+			+ " (SELECT COUNT(*) AS cnt FROM record"
 			+ " WHERE DATE(created_at)=DATE(NOW())-1"
 			+ " AND beacon_id"
-			+ " IN (SELECT id FROM beacon WHERE building_id=#{buildingId})")
-	@Results({
-		@Result(property="recordCount", column="count(*)"),
-		@Result(property="userCount", column="count(DISTINCT user_id)")
-	})
+			+ " IN (SELECT id FROM beacon WHERE building_id=#{buildingId})"
+			+ " GROUP BY DATE(created_at)) AS result")
 	RecordCount selectYesterdayRecordCountByBuildingId(@Param("buildingId") Long buildingId);
 	
 	// 해당 빌딩에 이번 주 동안 기록된 record 수 및 인원 수
-	@Select("SELECT count(*), count(DISTINCT user_id) FROM record"
+	@Select("SELECT IFNULL(SUM(result.cnt), 0) AS record_count, COUNT(result.cnt) AS user_count FROM"
+			+ " (SELECT COUNT(*) AS cnt FROM record"
 			+ " WHERE YEARWEEK(created_at, 7)=YEARWEEK(NOW(), 7)"
 			+ " AND beacon_id"
-			+ " IN (SELECT id FROM beacon WHERE building_id=#{buildingId})")
-	@Results({
-		@Result(property="recordCount", column="count(*)"),
-		@Result(property="userCount", column="count(DISTINCT user_id)")
-	})
+			+ " IN (SELECT id FROM beacon WHERE building_id=#{buildingId})"
+			+ " GROUP BY DATE(created_at)) AS result")
 	RecordCount selectWeeklyRecordCountByBuildingId(@Param("buildingId") Long buildingId);
 	
 	// 해당 빌딩에 지난 주 동안 기록된 record 수 및 인원 수
-	@Select("SELECT count(*), count(DISTINCT user_id) FROM record"
+	@Select("SELECT IFNULL(SUM(result.cnt), 0) AS record_count, COUNT(result.cnt) AS user_count FROM"
+			+ " (SELECT COUNT(*) AS cnt FROM record"
 			+ " WHERE YEARWEEK(created_at, 7)=YEARWEEK(NOW() - INTERVAL 1 WEEK, 7)"
 			+ " AND beacon_id"
-			+ " IN (SELECT id FROM beacon WHERE building_id=#{buildingId})")
-	@Results({
-		@Result(property="recordCount", column="count(*)"),
-		@Result(property="userCount", column="count(DISTINCT user_id)")
-	})
+			+ " IN (SELECT id FROM beacon WHERE building_id=#{buildingId})"
+			+ " GROUP BY DATE(created_at)) AS result")
 	RecordCount selectLastWeekRecordCountByBuildingId(@Param("buildingId") Long buildingId);
 	
 	// 해당 빌딩에 이번 달 동안 기록된 record 수 및 인원 수
-	@Select("SELECT count(*), count(DISTINCT user_id) FROM record"
+	@Select("SELECT IFNULL(SUM(result.cnt), 0) AS record_count, COUNT(result.cnt) AS user_count FROM"
+			+ " (SELECT COUNT(*) AS cnt FROM record"
 			+ " WHERE MONTH(created_at)=MONTH(NOW())"
 			+ " AND beacon_id"
-			+ " IN (SELECT id FROM beacon WHERE building_id=#{buildingId})")
-	@Results({
-		@Result(property="recordCount", column="count(*)"),
-		@Result(property="userCount", column="count(DISTINCT user_id)")
-	})
+			+ " IN (SELECT id FROM beacon WHERE building_id=#{buildingId})"
+			+ " GROUP BY DATE(created_at)) AS result")
 	RecordCount selectMonthlyRecordCountByBuildingId(@Param("buildingId") Long buildingId);
 	
 	// 해당 빋링에 저번 달 동안 기록된 record 수 및 인원 수
-	@Select("SELECT count(*), count(DISTINCT user_id) FROM record"
+	@Select("SELECT IFNULL(SUM(result.cnt), 0) AS record_count, COUNT(result.cnt) AS user_count FROM"
+			+ " (SELECT COUNT(*) AS cnt FROM record"
 			+ " WHERE MONTH(created_at)=MONTH(NOW() - INTERVAL 1 MONTH)"
 			+ " AND beacon_id"
-			+ " IN (SELECT id FROM beacon WHERE building_id=#{buildingId})")
-	@Results({
-		@Result(property="recordCount", column="count(*)"),
-		@Result(property="userCount", column="count(DISTINCT user_id)")
-	})
+			+ " IN (SELECT id FROM beacon WHERE building_id=#{buildingId})"
+			+ " GROUP BY DATE(created_at)) AS result")
 	RecordCount selectLastMonthRecordCountByBuildingId(@Param("buildingId") Long buildingId);
 	
 	
